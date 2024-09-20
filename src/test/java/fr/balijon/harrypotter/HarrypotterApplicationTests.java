5e544db210dd7ca4828327ad56ad666fe51b283b
IB <i.balijon@gmail.com>
2024-09-20 10:28:37 +0200

    initial commit

---
+++ src/test/java/fr/balijon/harrypotter/HarrypotterApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.balijon.harrypotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarrypotterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
